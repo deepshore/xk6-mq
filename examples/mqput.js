68024c575b304f64fa57d28d77c8a13deab58531
Selamanse <selamanse@scheinfrei.info>
2025-07-01 17:47:03 +0200

    putter example

--- examples/mqput.js
+++ examples/mqput.js
@@ -4,7 +4,8 @@ import Mq from 'k6/x/mq';
 export default function () {
     const url = "amqp://guest:guest@localhost:5672/"
     Mq.start()
-    Mq.put()
+    //Mq.put()
+    Mq.putter("./examples/test.zip")
   
   }
   
